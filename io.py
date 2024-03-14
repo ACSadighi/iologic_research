58b300a0895430d014be3acb6588f93fbca4eed9
ACSadighi <108032145+ACSadighi@users.noreply.github.com>
2024-03-14 11:50:41 -0700

    documentation

--- io.py
+++ io.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
 from z3 import *
 
+"""
+Currently, io.py only handles logic with the OR
+rule. The other rules can be applied similarly.
+"""
+
+# Parser
 prems = False
 conc = False
-prem_list = []      # List of premise norms
+prem_list = []
 conclusion = None
-fd = 'norms2.txt'
+fd = 'norms2.txt' # Norm file
 with open(fd, 'r') as f:
     for line in f:
         if line.strip() == '':
@@ -24,40 +30,41 @@ with open(fd, 'r') as f:
         elif 'Given Conclusion' in  line:
             conc = True
 
-# print(prem_list)
-# print(conclusion)
 
+# Applies resolution in solver s
 s = Solver()
-for p in prem_list:
+for p in prem_list: # Adds premises to solver
     print('Premise:', p)
     s.add(Implies(p[0],p[1]))
-print('Conclusion:', conclusion)
+# Adds conclusion to solver
 s.add(Not(Implies(conclusion[0],conclusion[1])))
-check = s.check()
+print('Conclusion:', conclusion)
+check = s.check() # Checks for satisfiability
 
+# Provable if unsat
 if str(check) == 'unsat':
     print('Provable')
 else:
+    # Not provable if sat
     print('Not provable')
+    # Adds norms until satisfiable
+    new_norms = []
     while str(check) != 'unsat':
+        # Gets model
         model = s.model()
-        print(model)
         literals = [v for v in model]
         vals = [model[v] for v in model]
-        # print(model)
-        # print(literals)
-        # print(vals)
-        new_norms = []
+        # Looks for norms that satisfy in resolution
         for i in range(len(vals)):
             if vals[i]:
+                # Adds premise
                 new_lit = literals[i]
-                # print(type(Bool(str(new_lit))))
                 new_norm = Implies(Bool(str(new_lit)),conclusion[1])
                 new_norms.append(new_norm)
                 s.add(new_norm)
                 check = s.check()
 
-        for i in new_norms:
-            print('Add norm: ', str(i)[7:])
-        check = s.check()
-#"""
+    # Prints new norms
+    for i in new_norms:
+        print('Add norm: ', str(i)[7:])
+    check = s.check()
