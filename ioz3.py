dbe65dff6281722651d6213a6bd58bc5a579b952
ACSadighi <108032145+ACSadighi@users.noreply.github.com>
2024-04-24 10:47:40 -0700

    cleanup

--- ioz3.py
+++ ioz3.py
@@ -6,7 +6,25 @@ Currently, io.py only handles logic with the OR
 rule. The other rules can be applied similarly.
 """
 
+
+def get_boolean_variables(expr):
+    variables = set()
+
+    def collect_variables(e):
+        if is_const(e):
+            if e.sort() == BoolSort():
+                variables.add(str(e))
+        else:
+            for arg in e.children():
+                collect_variables(arg)
+
+    collect_variables(expr)
+    return variables
+
+
 # Parser
+input_literals = set()
+output_literals = set()
 prems = False
 conc = False
 prem_list = []
@@ -21,14 +39,19 @@ with open(fd, 'r') as f:
             norm = [x.strip() for x in line.split(';')]
             for expression in norm:
                 expression = expression.replace('\n','')
+            tail = eval(norm[0])
+            head = eval(norm[1])
+            input_literals.update(get_boolean_variables(tail))
+            output_literals.update(get_boolean_variables(head))
             if prems:   # Adds premise norm to list
-                prem_list.append((eval(norm[0]),eval(norm[1])))
+                prem_list.append((tail,head))
             elif conc:  # Adds conclusion norm to list
-                conclusion = (eval(norm[0]),eval(norm[1]))
+                conclusion = ((tail,head))
         if 'Given Premises' in line:
             prems = True
         elif 'Given Conclusion' in  line:
             conc = True
+
 
 
 # Applies resolution in solver s
@@ -54,17 +77,27 @@ else:
         model = s.model()
         literals = [v for v in model]
         vals = [model[v] for v in model]
+        print(model)
+
         # Looks for norms that satisfy in resolution
-        for i in range(len(vals)):
-            if vals[i]:
-                # Adds premise
-                new_lit = literals[i]
-                new_norm = Implies(Bool(str(new_lit)),conclusion[1])
-                new_norms.append(new_norm)
-                s.add(new_norm)
-                check = s.check()
+        new_tail = Or()
+        new_head = And()
+        for lit, val in zip(literals, vals):
+            if val and str(lit) in input_literals:
+                new_tail = Or(new_tail, Bool(str(lit)))
+            elif not val and str(lit) in output_literals:
+                new_head = And(new_head, Bool(str(lit)))
+            else:
+                continue
+        
+        new_norm = Implies(simplify(new_tail), simplify(new_head))
+        print('New norm: ', str(new_norm)[7:], '\n')
+        new_norms.append(new_norm)
+        s.add(new_norm)
+        check = s.check()
 
     # Prints new norms
+    print()
     for i in new_norms:
         print('Add norm: ', str(i)[7:])
     check = s.check()
