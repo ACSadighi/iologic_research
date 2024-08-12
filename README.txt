23b65c3223356e9308c29c7ae685024a6a9539d8
ACSadighi <108032145+ACSadighi@users.noreply.github.com>
2024-08-12 16:12:34 -0700

    documentation

--- README.txt
+++ README.txt
@@ -7,11 +7,63 @@ the norms as boolean implications using Z3. If no
 proof can be found, the program prints the norms 
 required to complete the proof.
 
-To run the program, run the following code:
+To run the program, ensure that the Z3 Prover is built
+and installed in the parent of the current directory.
+To do this, visit the following link:
+https://github.com/Z3Prover/z3
+
+Once this is done, run the following code:
 ----
 python3 ioz3.py
 ----
-Five text files of norms are included and can be
+
+
+####################
+Norm File Notation
+####################
+Five text files of norms named 'normsk.txt' 
+(k being an integer 0-4) are included and can be 
 selected for parsing on line 33 of ioz3.py. The
 norms in each text file can also be altered 
 according to the user.
+
+For parsing purposes, instead of formulating a
+norm like 
+----
+(a, x)
+---- 
+as in customary I/O logic notation, norms in
+the text files are formulated like so:
+----
+Bool('a') ; Bool('x')
+----
+There are no parentheses enclosing the entire norm,
+a semicolon separates the norm body and head instead
+of a comma, and literals are expressed as "Bool('x')"
+instead of "x".
+
+To express a conjunctive clause, And() is used. Thus
+if "^" is the logical conjunction symbol, the norm
+----
+(a ^ b, x)
+----
+is instead formulated as
+----
+And( Bool('a'), Bool('b') ) ; x
+----
+Similarly, to express a disjunctive clause, Or() is used. Thus
+if "v" is the logical disjunction symbol, the norm
+----
+(a v b, x)
+----
+is instead formulated as
+----
+Or( Bool('a'), Bool('b') ) ; x
+----
+
+In these text files, premise norms are entered
+directly under the line that says 'Given Premises'
+with no lines skipped. below the premise norms, a
+blank linke must be preserved before the line with
+'Given Conclusion', under which the conclusion norm
+is entered.
--- ioz3.py
+++ ioz3.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 from z3 import *
 
-"""
-Change the norm file on line 32 to see how different
-rules are handled. Add, remove, or alter the norms
-as needed.
-"""
-
-
 def get_boolean_variables(expr):
+    """
+    Helper function for the parser to incorporate
+    literals from norm heads/tails in their
+    respective sets.
+    """
     variables = set()
 
     def collect_variables(e):
@@ -23,82 +21,126 @@ def get_boolean_variables(expr):
     return variables
 
 
-# Parser
-input_literals = set()
-output_literals = set()
+"""
+Variables for norms/literals involved in proof are initialized
+"""
+body_literals = set()   # Set of variables that appear in norm bodies
+head_literals = set()   # Set of variables that appear in norm heads
+prem_list = []          # Set of premise norms
+conclusion = None       # Conclusion norm
+
+"""
+Parser reads text files to populate norm/literal variables
+"""
 prems = False
 conc = False
-prem_list = []
-conclusion = None
-fd = 'norms3.txt' # Norm file
+fd = 'norms3.txt' # Norm file to be read
 with open(fd, 'r') as f:
     for line in f:
-        if line.strip() == '':
+        if line.strip() == '':  # Stops parsing norms
             prems = False
             conc = False
-        if prems or conc:
+        if prems or conc:   # Norm is parsed
+            # Processes strings
             norm = [x.strip() for x in line.split(';')]
-            for expression in norm:
+            for expression in norm: #
                 expression = expression.replace('\n','')
-            tail = eval(norm[0])
+            # Converts strings to Z3 expressions
+            body = eval(norm[0])
             head = eval(norm[1])
-            input_literals.update(get_boolean_variables(tail))
-            output_literals.update(get_boolean_variables(head))
+            # Adds literals to body/head sets
+            body_literals.update(get_boolean_variables(body))
+            head_literals.update(get_boolean_variables(head))
+            # Stores norms as tuples
             if prems:   # Adds premise norm to list
-                prem_list.append((tail,head))
-            elif conc:  # Adds conclusion norm to list
-                conclusion = ((tail,head))
+                prem_list.append((body,head))
+            elif conc:  # Sets conclusion norm
+                conclusion = ((body,head))
+
         if 'Given Premises' in line:
-            prems = True
+            prems = True    # Prepares to parse premise norms
         elif 'Given Conclusion' in  line:
-            conc = True
+            conc = True     # Prepares to parse conclusion norm
 
 
 
-# Applies resolution in solver s
-s = Solver()
-for p in prem_list: # Adds premises to solver
-    print('Premise:', p)
-    s.add(Implies(p[0],p[1]))
-# Adds conclusion to solver
-s.add(Not(Implies(conclusion[0],conclusion[1])))
-print('Conclusion:', conclusion)
-check = s.check() # Checks for satisfiability
+"""
+Resolution is applied to the to the premise and
+conclusion norms using the Z3 proof solver. 
+Norms are added to the solver as Z3 'Implies' 
+expressions (i.e. conditional expressions).
+"""
+# Initializes Z3 solver
+solver = Solver()
+# Adds premise norms from list to solver
+for p in prem_list: 
+    solver.add(Implies(p[0],p[1]))
+    print('Premise:', p) # Displays premise norm
+# Adds negated conclusion norm to solver
+solver.add(Not(Implies(conclusion[0],conclusion[1])))
+print('Conclusion:', conclusion) # Displays conslusion norm
 
-# Provable if unsat
+"""
+Norms in the solver are checked for satisfiability.
+"""
+check = solver.check()
+
+"""
+If the solver returns 'unsat', a proof exists for the
+given conclusion and premise norms.
+
+If the solver returns 'sat', a proof does not exist.
+The model from the solver that returns 'sat' is used
+to construct a premise norm to add to the solver.
+Premise norms are added to the solver until a 
+proof exists and the solver returns 'unsat'.
+"""
 if str(check) == 'unsat':
+    # Displays that proof exists
     print('Provable')
 else:
-    # Not provable if sat
+    # Not provable if 'sat'
     print('Not provable\n')
-    # Adds norms until satisfiable
-    new_norms = []
-    while str(check) != 'unsat':
-        # Gets model
-        model = s.model()
-        literals = [v for v in model]
-        vals = [model[v] for v in model]
-        print('Model:', model)
 
-        # Looks for norms that satisfy in resolution
-        new_tail = Or()
+    new_norms = [] # Initializes list of norms to be added
+    # New norms are found and added until solver returns 'unsat'
+    while str(check) != 'unsat':
+        # Gets model that satisfies norms
+        model = solver.model()
+        literals = [v for v in model]    # List of literals
+        vals = [model[v] for v in model] # Truth values of literals
+
+        # Uncomment print statement below to view model
+        # print('Model:', model)
+
+        # # #
+        # Constructs a new norm using the model, iterating
+        # through all literals in all norms. If a literal
+        # is a body literal and true in the model, or a head
+        # literal and false in the mode, the literal is 
+        # included in the new norm.
+        # # #
+        new_body = Or()
         new_head = And()
+        # Checks all literals that appear in all norms
         for lit, val in zip(literals, vals):
-            if val and str(lit) in input_literals:
-                new_tail = Or(new_tail, Bool(str(lit)))
-            elif not val and str(lit) in output_literals:
+            # Adds body literal to new norm if it is true in the model
+            if val and str(lit) in body_literals:
+                new_body = Or(new_body, Bool(str(lit)))
+            # Adds head literal to new norm if it is false in the model
+            elif not val and str(lit) in head_literals:
                 new_head = And(new_head, Bool(str(lit)))
             else:
                 continue
-        
-        new_norm = Implies(simplify(new_tail), simplify(new_head))
-        print('New norm: ', str(new_norm)[7:], '\n')
+        new_norm = Implies(simplify(new_body), simplify(new_head))
         new_norms.append(new_norm)
-        s.add(new_norm)
-        check = s.check()
+        # Uncomment print statement below to see new norm
+        # print('New norm: ', str(new_norm)[7:], '\n')
+        
+        # Adds new norm to solver, checks if 'sat'
+        solver.add(new_norm)
+        check = solver.check()
 
     # Prints new norms
-    print()
     for i in new_norms:
         print('Add norm: ', str(i)[7:])
-    check = s.check()
